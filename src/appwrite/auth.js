1f7a572f40c4645d1c441047808ccede68c4734e
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2023-11-29 20:17:51 +0530

    Created New File and do some change

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -67,3 +67,5 @@ const authService =new AuthService();
 
 export default authService; 
 
+
+
---
+++ src/appwrite/cofig.js
@@ -0,0 +1,9 @@
+import { Service } from 'appwrite/types/service.js';
+import conf from '../conf/conf.js';
+import {Client, ID, Databases, Storage, Query} from "appwrite";
+
+
+ export class Service{}
+
+const service = new Service()
+ export default service
