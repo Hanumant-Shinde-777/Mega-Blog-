d50a1d1e8de9bcf139576f9d2997ee3e62e394ca
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2023-12-05 13:16:46 +0530

    Changes in Index and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-
+import React, {useState, useEffect} from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+
 
 function App() {
   
-console.log( );
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  //For the useEffect 
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
   return (
    <>
    <h1> A blog with appwrite </h1>
