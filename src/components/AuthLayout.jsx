92cf1f342e45fd79acdcf4469ceaed17a666c5ce
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2023-12-06 17:20:47 +0530

    Changes

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -9,15 +9,6 @@ export default function Protected({children, authentication = true}) {
     const authStatus = useSelector(state => state.auth.status)
 
     useEffect(() => {
-        //TODO: make it more easy to understand
-
-        // if (authStatus ===true){
-        //     navigate("/")
-        // } else if (authStatus === false) {
-        //     navigate("/login")
-        // }
-        
-        //let authValue = authStatus === true ? true : false
 
         if(authentication && authStatus !== authentication){
             navigate("/login")
@@ -29,4 +20,3 @@ export default function Protected({children, authentication = true}) {
 
   return loader ? <h1>Loading...</h1> : <>{children}</>
 }
-//6.27.09
